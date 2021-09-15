dcaa4fae96c22d948a77f6cde9b80224fdc11813
nazar.bigun <nazar.bigun@gmaiil.com>
2021-09-15 15:36:21 +0100

    build(app): generate auth module
    generate auth module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TimeModule } from './time/time.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TimeModule],
+  imports: [TimeModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
