ac325011b6eea22b4d83d68a8a81169a7e81344d
nazar.bigun <nazar.bigun@gmaiil.com>
2021-09-15 15:35:59 +0100

    build(app): generate time module
    generate time module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TimeModule } from './time/time.module';
 
 @Module({
-  imports: [],
+  imports: [TimeModule],
   controllers: [AppController],
   providers: [AppService],
 })
