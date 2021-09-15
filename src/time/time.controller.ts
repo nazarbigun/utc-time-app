05cdcfdc475164d78f908931848f2a0bb1905521
nazar.bigun <nazar.bigun@gmaiil.com>
2021-09-15 16:05:09 +0100

    build(app): add open api
    add open api, add open api auto generation tool

--- src/time/time.controller.ts
+++ src/time/time.controller.ts
@@ -4,11 +4,12 @@ import {
   Get,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { TimeService } from './time.service';
 import { UtcTimeView } from './views/utc-time.view';
 
 @Controller('time')
+@ApiTags('time')
 export class TimeController {
   constructor(private readonly timeService: TimeService) {}
 
